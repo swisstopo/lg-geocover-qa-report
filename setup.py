6311d2c91be52e372c58a8c8ea4f098a3296ccaf
Marc Monnerat <procrastinatio@gmail.com>
2025-01-10 21:50:00 +0100

    tagging

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     use_scm_version={
         # 'root': '.', # GIT root
         'write_to': 'src/geocover_qa/_version.py',
-        'version_scheme': 'post-release',
-        'local_scheme': 'dirty-tag', 
+        'version_scheme': "release-branch-semver", # 'post-release',
+        'local_scheme': "no-local-version",  #'dirty-tag', 
         'fallback_version': '0.1'
     },
     name="geocover-qa",
